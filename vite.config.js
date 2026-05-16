55e60ceaa7734dcb266c3e7226aad1a825671cfe
Murilo Pequeno <murilinho.pequeno@gmail.com>
2026-05-16 19:44:27 -0300

    build: configura dependencias

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600, 700],
+                }),
+                bunny('Caveat', {
+                    weights: [600, 700],
+                }),
+                bunny('Fraunces', {
+                    weights: [600, 700],
+                }),
+            ],
+        }),
+        react(),
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
